526f5a5474847f11179c09f755a85849440716af
specialmindsaarhus <161840253+specialmindsaarhus@users.noreply.github.com>
2024-11-04 08:44:37 +0000

    updated for the students

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,5 +1,16 @@
-//ignorer import, det er ikke vigtigt til en start...
-import { url, requestOptions } from "./fetchData.js";
+export const url =
+  "https://api.thecatapi.com/v1/images/search?size=small&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1";
+
+const headers = new Headers({
+  "Content-Type": "application/json",
+  "x-api-key": "DEMO-API-KEY",
+});
+
+export var requestOptions = {
+  method: "GET",
+  headers: headers,
+  redirect: "follow",
+};
 
 export function fetchCat() {
   fetch(url, requestOptions)
